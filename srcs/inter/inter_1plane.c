a461eda4dbbd3b0fba7c60a7c6b763d6e7a65cb4
Lucie Le Briquer <contact@lucie-lebriquer.fr>
2020-12-22 00:25:08 +0100

    ok except triangle, normale too save

--- srcs/inter/inter_1plane.c
+++ srcs/inter/inter_1plane.c
@@ -6,7 +6,7 @@
 /*   By: lle-briq <lle-briq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 05:44:34 by lle-briq          #+#    #+#             */
-/*   Updated: 2020/12/21 05:44:35 by lle-briq         ###   ########.fr       */
+/*   Updated: 2020/12/22 00:23:38 by lle-briq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,10 @@ float	inter_1plane(t_ray ray, t_obj obj)
 	float	t;
 	float	sc;
 
-	sc = dot(obj.axe, obj.o);
-	if (sc != 0)
-	{
-		t = sc / dot(obj.axe, ray.dir);
-		if (t > 0)
-			return (t);
-	}
+	//sc = dot(obj.axe, obj.o);
+	sc = dot(obj.axe, sub_vect(obj.o, ray.org));
+	t = sc / dot(obj.axe, ray.dir);
+	if (t > 0)
+		return (t);
 	return (-1);
 }
--- srcs/inter/inter_4triangle.c
+++ srcs/inter/inter_4triangle.c
@@ -6,15 +6,65 @@
 /*   By: lle-briq <lle-briq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 05:45:12 by lle-briq          #+#    #+#             */
-/*   Updated: 2020/12/21 05:49:59 by lle-briq         ###   ########.fr       */
+/*   Updated: 2020/12/22 00:24:33 by lle-briq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-float	inter_4triangle(t_ray ray, t_obj obj)
+/*
+**  A		B		C
+**	o		axe		p
+*/
+
+float	inter_plane_trg(t_ray ray, t_obj trg)
 {
-	(void)ray;
-	(void)obj;
+	float	t;
+	float	sc;
+	t_vect	normale;
+	
+	normale = prod_vect(sub_vect(trg.axe, trg.o), sub_vect(trg.p, trg.o));
+	normalize(&normale);
+	sc = dot(sub_vect(trg.p, ray.org), normale);
+	t = sc / dot(ray.dir, normale);
+	if (t > 0)
+		return (t);
+	return (-1);
+}
+
+int		is_in_trg(t_vect u, t_vect v, t_vect w)
+{
+	float	det;
+	float	alpha;
+	float	beta;
+	float	gamma;
+
+	det = norm2(u) * norm2(v) - 2 * dot(u, v);
+	beta = (dot(w, u) * norm2(v) - dot(v, u) * dot(w, v)) / det;
+	gamma = (norm2(u) * dot(w, v) - dot(v, u) * dot(w, u)) / det;
+	alpha = 1 - beta - gamma;
+	if (alpha > 0 && beta > 0 && gamma > 0 && alpha <= 1 && beta <= 1 
+	&& gamma <= 1)
+		return (1);
+	return (-1);
+}
+
+float	inter_4triangle(t_ray ray, t_obj trg)
+{
+	t_vect	u;
+	t_vect	v;
+	t_vect	w;
+	t_vect	point;
+	float	t;
+
+	t = inter_plane_trg(ray, trg);
+	if (t < 0)
+		return (-1);
+	point = add_vect(ray.org, mul_vect(t, ray.dir));
+	u = sub_vect(trg.axe, trg.o);
+	v = sub_vect(trg.p, trg.o);
+	w = sub_vect(point, trg.o);
+	if (is_in_trg(u, v, w))
+		return (t);
 	return (-1);
 }
