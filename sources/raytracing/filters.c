669ed0a50c423f0e5fe25d7e60439654dde571de
Lucie Le Briquer <contact@lucie-lebriquer.fr>
2021-01-09 23:44:03 +0100

    norme

--- sources/raytracing/filters.c
+++ sources/raytracing/filters.c
@@ -6,7 +6,7 @@
 /*   By: lle-briq <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 15:28:41 by lle-briq          #+#    #+#             */
-/*   Updated: 2021/01/08 15:53:38 by lle-briq         ###   ########.fr       */
+/*   Updated: 2021/01/09 23:43:46 by lle-briq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,21 @@ int	black_white(int color)
 	int	moy;
 
 	rgb[0] = (color / 256 / 256);
-	rgb[1] = ((color / 256) %  256);
-	rgb[2] = (color %  256);
+	rgb[1] = ((color / 256) % 256);
+	rgb[2] = (color % 256);
 	moy = (rgb[0] + rgb[1] + rgb[2]) / 3;
 	return (moy * 65793);
 }
 
-int sepia(int color)
+int	sepia(int color)
 {
 	int		rgb[3];
 	double	sep[3];
 	int		res[3];
 
 	rgb[0] = (color / 256 / 256);
-	rgb[1] = ((color / 256) %  256);
-	rgb[2] = (color %  256);
+	rgb[1] = ((color / 256) % 256);
+	rgb[2] = (color % 256);
 	sep[0] = 0.393 * rgb[0] + 0.769 * rgb[1] + 0.189 * rgb[2];
 	sep[1] = 0.349 * rgb[0] + 0.686 * rgb[1] + 0.168 * rgb[2];
 	sep[2] = 0.272 * rgb[0] + 0.534 * rgb[1] + 0.131 * rgb[2];
@@ -42,11 +42,11 @@ int sepia(int color)
 	return (256 * 256 * res[0] + 256 * res[1] + res[2]);
 }
 
-int compo(int color, int i)
+int	compo(int color, int i)
 {
 	if (i == 0)
-		return ((color / 256 / 256) * 256 * 256);
+		return (256 * 256 * (color / 256 / 256));
 	if (i == 1)
-		return (((color / 256) % 256) * 256);
+		return (256 * ((color / 256) % 256));
 	return (color % 256);
 }
--- sources/raytracing/inter.c
+++ sources/raytracing/inter.c
@@ -6,7 +6,7 @@
 /*   By: lle-briq <lle-briq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 05:43:41 by lle-briq          #+#    #+#             */
-/*   Updated: 2021/01/09 23:30:18 by lle-briq         ###   ########.fr       */
+/*   Updated: 2021/01/09 23:42:01 by lle-briq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,20 @@
 static t_vect	normale_type(t_vect p, t_obj obj, t_inter itr)
 {
 	t_vect	n;
+	t_vect	tmp;
 
 	if (obj.type == SPHERE)
 		n = sub_vect(p, obj.o);
 	else if (obj.type == CYLINDER)
-		n = sub_vect(p, add_vect(mul_vect(dot(sub_vect(p, obj.o), obj.axe), obj.axe), obj.o));
+	{
+		tmp = mul_vect(dot(sub_vect(p, obj.o), obj.axe), obj.axe);
+		n = sub_vect(p, add_vect(tmp, obj.o));
+	}
 	else if (obj.type == CONE)
-		init_vect(&n, p.x - obj.o.x, p.y - obj.o.y, obj.r * obj.r * (p.z - obj.o.z) / (obj.h * obj.h));
+	{
+		tmp.z = obj.r * obj.r * (p.z - obj.o.z) / (obj.h * obj.h);
+		init_vect(&n, p.x - obj.o.x, p.y - obj.o.y, tmp.z);
+	}
 	else
 		n = obj.axe;
 	if (dot(itr.ray.dir, n) > 0)
