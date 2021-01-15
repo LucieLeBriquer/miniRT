18ec4205c87ec02f05a5e7ff228ddc053c8eccd3
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2021-01-15 03:08:09 +0100

    clean mlx keycode

--- includes/mlx_code.h
+++ includes/mlx_code.h
@@ -6,59 +6,18 @@
 /*   By: lle-briq <lle-briq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 22:13:39 by lle-briq          #+#    #+#             */
-/*   Updated: 2021/01/11 17:18:12 by lle-briq         ###   ########.fr       */
+/*   Updated: 2021/01/15 03:07:45 by lle-briq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MLX_CODE_H
 # define MLX_CODE_H
-
-# ifdef MAC
-#  define DESTROY 10
-# else
-#  define DESTROY 33
-# endif
-
-# ifdef MAC
-#  define S_KEY 1
-# else
-#  define S_KEY 115
-# endif
-
-# ifdef MAC
-#  define W_KEY 13
-# else
-#  define W_KEY 122
-# endif
-
-# ifdef MAC
-#  define SP_KEY 49
-# else
-#  define SP_KEY 32
-# endif
-
-# ifdef MAC
-#  define ESC_KEY 53
-# else
-#  define ESC_KEY 65307
-# endif
-
-# ifdef MAC
-#  define RIGHT_KEY 124
-# else
-#  define RIGHT_KEY 65363
-# endif
-
-# ifdef MAC
-#  define LEFT_KEY 123
-# else
-#  define LEFT_KEY 65361
-# endif
-
-# ifdef MAC
-#  define F_KEY 3
-# else
-#  define F_KEY 102
-# endif
-
+# define DESTROY 33
+# define S_KEY 115
+# define W_KEY 122
+# define SP_KEY 32
+# define ESC_KEY 65307
+# define RIGHT_KEY 65363
+# define LEFT_KEY 65361
+# define F_KEY 102
 #endif
